5f03c12aae8584f45720096f3100ba32d398490e
Aman Sharma <writetoaman99@gmail.com>
2019-12-28 11:37:01 +0530

    Added content to home screen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,23 +1,115 @@
-import React from 'react';
-import {View,Text,StyleSheet,Button} from 'react-native';
+// import React from 'react';
+// import {View,Text,StyleSheet,Button} from 'react-native';
 
-const HomeScreen = (props)=>{
-    return(
-        <View style={styles.screen}>
-            <Text>Home Screen</Text>
-            <Button title="Press me" onPress={
-                ()=>{props.navigation.navigate({routeName:'Result'})}
-            }/>
-        </View>
-    )
+// const HomeScreen = (props)=>{
+//     return(
+//         <View style={styles.screen}>
+//             <Text>Home Screen</Text>
+//             <Button title="Press me" onPress={
+//                 ()=>{props.navigation.navigate({routeName:'Result'})}
+//             }/>
+//         </View>
+//     )
+// }
+
+// const styles = StyleSheet.create({
+//     screen:{
+//         flex:1,
+//         alignItems:'center',
+//         justifyContent:'center'
+//     }
+// })
+
+// export default HomeScreen;
+import React from 'react';
+import {KeyboardAvoidingView, Image, StyleSheet, Alert} from 'react-native';
+import { Input, Button} from 'react-native-elements';
+
+class Search extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            text: ''
+        };
+    }
+
+    static navigationOptions = {
+        header : null
+    };
+
+    searchReddit(e){
+        if(this.state.text.length){
+            this.props.navigation.navigate('Result', {
+                subreddit: this.state.text,
+            });
+        }else{
+            Alert.alert(
+                'Invalid Input',
+                'Please Enter Something',
+                [
+                    {text: 'OK'}
+                ]
+            )
+        }
+    }
+
+    render() {
+        return (
+            <KeyboardAvoidingView
+                style={styles.view}
+                behavior="padding"
+            >
+
+                <Image
+                    source={require('../res/reddit.png')}
+                    style={{
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                        width: '100%',
+                        height: 200,
+                    }}
+                />
+
+                <Input
+                    inputContainerStyle={styles.search}
+                    containerStyle={styles.container}
+                    inputStyle={styles.input}
+                    onChangeText={(text) => this.setState({text})}
+                    placeholder='Search Image '
+                />
+
+                        
+                <Button
+                    title='Search'
+                    buttonStyle={styles.button}
+                    onPress={this.searchReddit.bind(this)}
+                />
+
+            </KeyboardAvoidingView>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-    screen:{
-        flex:1,
-        alignItems:'center',
-        justifyContent:'center'
-    }
-})
+   view:{
+       flex:1,
+       alignItems:'center',
+       justifyContent:'center'
+   },
+   search:{
+    height:50
+   },
+   container:{
+    paddingHorizontal:'10%',
+    paddingTop:10
+   },
+   input:{},
+   button:{
+    alignItems:'center',
+    paddingHorizontal:20,
+    marginVertical:10
+   } 
+});
 
-export default HomeScreen;
+
+export default Search;
